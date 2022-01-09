c468f44fdc869cf1ce4593119513033f5b7be35a
BronnikSB <sguattero516@mail.ru>
2022-01-09 19:25:39 +0300

    Task 1(Убрал лишнее)

--- main.py
+++ main.py
@@ -42,19 +42,5 @@ Quantity_file_pathPy.write(f'Количество папок: {counter_folder}\n
                            f'Количество файлов с расширением ".py": {counter_file_py}')
 Quantity_file_pathPy.close()
 
-from atf.ui import *
-
-
-class AuthPage(Region):
-
-    login = Element(By.CSS_SELECTOR, '[name="login"]', 'Логин')
-    password = Element(By.CSS_SELECTOR, '[name="password"]', 'Пароль')
-    enter_btn = Element(By.CSS_SELECTOR, '.auth-Form__submit  default', 'Войти')
-
-    def auth(self, user_login, user_password):
-        self.login.type_in(user_login)
-        self.password.type_in(user_password)
-        self.enter_btn.click()
-
 
 
