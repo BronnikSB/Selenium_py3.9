d4d9123cb3f4c706a2732a8740e6e0309b6a34fc
BronnikSB <sguattero516@mail.ru>
2022-06-07 23:58:32 +0300

    lesson_237240(Правки)

--- Pytest_lesson3.6/test_items.py
+++ Pytest_lesson3.6/test_items.py
@@ -1,12 +1,11 @@
 import time
-
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
+# Проверка наличия кнопки добаления в корзину.
 def test_interface_lang(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    btn_add_to_basket = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form")
+    btn_add_to_basket = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form", "Добавить в корзину")
     assert len(btn_add_to_basket) > 0
     time.sleep(15)
