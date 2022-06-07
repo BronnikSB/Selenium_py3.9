57918322f6159a789da2d29bf935baaa327b574f
BronnikSB <sguattero516@mail.ru>
2022-06-08 00:07:09 +0300

    lesson_237240(Правки 0.1)

--- Pytest_lesson3.6/test_items.py
+++ Pytest_lesson3.6/test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 # Проверка наличия кнопки добаления в корзину.
-def test_interface_lang(browser):
+def test_btn_add_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     btn_add_to_basket = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form", "Добавить в корзину")
